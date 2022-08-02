054b2fb9778afc67807d2783b7a981542c662063
mitakawe123 <dkyuchukov2003@gmail.com>
2022-08-02 14:43:24 +0300

    crash course

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private tasksService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasksService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  deleteTask(task: Task): void {
+    this.tasksService.getTasks().subscribe(() => {
+      this.tasksService
+        .deleteTask(task)
+        .subscribe(
+          () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+        );
+    });
+  }
+}
